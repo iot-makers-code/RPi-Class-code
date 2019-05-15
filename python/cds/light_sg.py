c12d9d7b30e448b280ee92fc808b22c60f5adf6d
Yongsoo Han <ilovecho@gmail.com>
2019-05-15 22:07:14 +0900

    Create light_sg.py

---
+++ python/cds/light_sg.py
@@ -0,0 +1,34 @@
+from turtle import *
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BCM)
+print("Show sensor digital data")
+GPIO.setup(24, GPIO.IN)
+
+colors=['white', 'red', 'green', 'yellow']
+pre=0; page=0; x=0
+while x<1200:
+    if x % 150 == 0:
+        page = 0 if page else 1
+        clearscreen();bgcolor('black');
+        pencolor('white');speed(0)
+        up();rt(-90);fd(90);
+        rt(-90);fd(150);rt(180);down()
+
+    inx = GPIO.input(24)
+    pencolor(colors[page*2+inx])
+    if inx == pre : fd(10)
+    if pre == 0 and inx == 1 : 
+        rt(-90);fd(10);rt(90);fd(10)
+    if pre == 1 and inx == 0 : 
+        rt(90);fd(10);rt(-90);fd(10)
+    pre = inx
+
+    if x % 30 == 29:
+        up(); rt(90);fd(30);
+        rt(90);fd(30*10);rt(180); down();
+
+    time.sleep(0.01)
+    x=x+1
