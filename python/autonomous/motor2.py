5d36f09b65b2a5502bc0dfd9b88a4d2193528c05
Yongsoo Han <ilovecho@gmail.com>
2019-05-15 23:13:10 +0900

    Update motor2.py

--- python/autonomous/motor2.py
+++ python/autonomous/motor2.py
@@ -5,22 +5,42 @@ import time
 pin1 = 23
 pin2 = 24
 
-forward=PWMOutputDevice(pin1, True, 0, 1000)
-reverse=PWMOutputDevice(pin2, True, 0, 1000)
+pin3 = 17
+pin4 = 27
+
+forwardRight=PWMOutputDevice(pin1, True, 0, 1000)
+reverseRight=PWMOutputDevice(pin2, True, 0, 1000)
+
+forwardLeft=PWMOutputDevice(pin3, True, 0, 1000)
+reverseLeft=PWMOutputDevice(pin4, True, 0, 1000)
 
 try:
-	forward.value=1
-	reverse.value=0
-	time.sleep(1)
-
-	forward.value=0.5
-	reverse.value=0
-	time.sleep(1)
+	forwardRight.value=1
+	reverseRight.value=0
+	forwardLeft.value=1
+	reverseLeft.value=0
+	time.sleep(0.5)
 	
-	forward.value=0
-	reverse.value=1
-	time.sleep(1)
-
+	forwardRight.value=0
+	reverseRight.value=1
+	forwardLeft.value=0
+	reverseLeft.value=1
+	time.sleep(0.5)
+                                                             
+        forwardRight.value=0        
+      reverseRight.value=0
+        forwardLeft.value=1
+        reverseLeft.value=0
+        time.sleep(1)
+                                                             
+        forwardRight.value=1
+        reverseRight.value=0
+        forwardLeft.value=0
+        reverseLeft.value=0
+        time.sleep(1)
+       
 finally:
-	forward.value=0
-	reverse.value=0
+      forwardRight.value=0
+      reverseRight.value=0
+      forwardLeft.value=0
+      reverseLeft.value=0
