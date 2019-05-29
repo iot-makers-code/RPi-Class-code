3f0b515f5b3ecb3c61b9a836b28a30406f0bf407
Yongsoo Han <ilovecho@gmail.com>
2019-05-30 08:55:26 +0900

    Create dsm501a01.ino

---
+++ nodemcu/dust/dsm501a/dsm501a01.ino
@@ -0,0 +1,41 @@
+// Connect the Pin_3 of DSM501A to Arduino 5V
+// Connect the Pin_5 of DSM501A to Arduino GND
+// Connect the Pin_2 of DSM501A to Arduino D8
+// www.elecrow.com
+#include<string.h>
+byte buff[2];
+int pin = 15;//DSM501A input D8
+unsigned long duration;
+unsigned long starttime;
+unsigned long endtime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+ 
+int i=0;
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(pin,INPUT);
+  starttime = millis(); 
+}
+void loop()
+{
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy += duration;
+  endtime = millis();
+  if ((endtime-starttime) > sampletime_ms)
+  {
+    ratio = (lowpulseoccupancy-endtime+starttime + sampletime_ms)/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print("lowpulseoccupancy:");
+    Serial.print(lowpulseoccupancy);
+    Serial.print("    ratio:");
+    Serial.print(ratio);
+    Serial.print("    DSM501A:");
+    Serial.println(concentration);
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  } 
+}
