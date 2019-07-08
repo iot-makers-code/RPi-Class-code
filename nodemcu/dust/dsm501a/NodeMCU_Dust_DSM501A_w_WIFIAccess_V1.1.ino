accc4683fd55172c1714419648001498063a0eb3
Yongsoo Han <ilovecho@gmail.com>
2019-07-08 16:53:05 +0900

    AC기능을 가진 미세먼지센서
    미세먼지측정 NodeMCU 사용방법 요약
    ------------------------------------------------------
    (1) 먼지센서와 NodeMCU(mini)를 연결한다.
    (2) NodeMCU를 컴퓨터에 USB케이블로 연결한다
    (3) NodeMCU에 먼지측정 프로그램을 업로드 한다.
    (4) NodeMCU를 스마트폰으로 연결한다.(WiFi: DUST-****, 12345678)
    (5) 스마트폰 웹브라우저로 데이터를 가져온다(http://192.168.4.1/get)
    (6) 수집데이터를 스마트폰에 저장한다(예, 카톡, 메일, 등)
    
    참조자료: 사물코딩(말랑820-nodeMCU-미세먼지) 학습자료, @네이버카페 "코딩 성공하면 잼있다"

---
+++ nodemcu/dust/dsm501a/NodeMCU_Dust_DSM501A_w_WIFIAccess_V1.1.ino
@@ -0,0 +1,159 @@
+// https://www.elecrow.com/wiki/index.php?title=Dust_Sensor-_DSM501A
+// Connect the Pin_3 of DSM501A to nodeMCU 5V(Vin)
+// Connect the Pin_5 of DSM501A to nodeMCU GND
+// Connect the Pin_2 of DSM501A to nodeMCU D8
+// www.elecrow.com
+// http://blog.naver.com/PostView.nhn?blogId=twophase&logNo=220709172472
+#include <ESP8266WiFi.h>
+#include<string.h>
+
+char  ssid[32];
+const char* password = "12345678";
+WiFiServer server(80);
+
+int pin = 15;//DSM501A input on NOdeMCU D8
+unsigned long duration;
+unsigned long starttime;
+unsigned long endtime;
+unsigned long sampletime_ms = 30000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+float realratio = 0;
+
+
+#define buf_size 2880
+typedef struct _value {
+	long ts;
+	int  ds;
+} Value;
+
+Value buf[buf_size];
+unsigned int  buf_idx = 0;
+char str[256];
+ 
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(pin,INPUT);
+
+  setupWiFi();
+  server.begin();
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+    
+
+  memset(buf, 0x00, sizeof(buf));
+  //for(int i=0; i< buf_size; i++) buf[i]=0;
+  starttime = millis(); 
+}
+void loop()
+{
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    duration = pulseIn(pin, LOW);
+    lowpulseoccupancy += duration;
+    endtime = millis();
+    if ((endtime-starttime) > sampletime_ms)
+    {
+      ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+      realratio = ratio/100;
+      concentration = 1.1*pow(realratio,3)-3.8*pow(realratio,2)+520*realratio+0.62; // using spec sheet curve
+      buf[buf_idx].ts = endtime;
+      buf[buf_idx].ds = (int)concentration;
+      Serial.print(buf[buf_idx].ds);
+      buf_idx++;
+      if (buf_idx >= buf_size) buf_idx = 0;
+      lowpulseoccupancy = 0;
+      Serial.println("");
+      starttime = millis();
+      }
+  } else { 
+
+    // Read the first line of the request
+    String req = client.readStringUntil('\r');
+    Serial.println(req);
+    client.flush();
+
+    client.print(
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n\r\n"
+		"<!DOCTYPE HTML>\r\n<html>\r\n"
+		"<meta name='viewport' content='width=device-width, initial-scale=1.0'>\r\n"
+		"<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>\r\n"
+		"<title>Air Concenturation</title>\r\n"
+		"</head>\r\n<body>\r\n"
+	  );
+      sprintf(str, "<div>Last Time:<span id='ct'>%ld</span>, Count:<span id='cp'>%d</span></div>\r\n", millis(), buf_idx); 
+      client.print(str);
+      
+    if (req.indexOf("/get") == -1) {
+      client.print("get");;
+    } else {
+      int i;
+      for (i = buf_idx; i<buf_size;i++) {
+      	if (buf[i].ts==0) continue;
+      	sprintf(str, "<div>%s\t<span class='ts'>%ld</span>\t<span class='ds'>%d</span></div>\r\n", ssid, buf[i].ts, buf[i].ds); 
+      	//client.print(String(str))
+      	client.print(str);
+      }
+      for (i = 0; i<(int)buf_idx; i++) {
+      	if (buf[i].ts==0) continue;
+      	sprintf(str, "<div>%s\t<span class='ts'>%ld</span>\t<span class='ds'>%d</span></div>\r\n", ssid, buf[i].ts, buf[i].ds); 
+      	//client.print(String(str))
+      	client.print(str);
+      }
+    } 
+
+    client.print(
+		"<div style='font-size:0.8em;'>(c)2019 <em>anhive</em> All Rights Reserved.</div>\r\n"
+		"<script>\r\n"
+		"Date.prototype.yyyymmdd = function() {"
+		"var mm = this.getMonth() + 1;"
+		"var dd = this.getDate();"
+		"var hh = this.getHours();"
+		"var ii = this.getMinutes();"
+		"var ss = this.getSeconds();"
+		"return ["
+			"(mm>9 ? '' : '0') + mm,"
+			"(dd>9 ? '/' : '/0') + dd,"
+			"(hh>9 ? ' ' : ' 0') + hh,"
+			"(ii>9 ? ':' : ':0') + ii,"
+			"(ss>9 ? ':' : ':0') + ss"
+			"].join('');"
+		"};\r\n"
+		""
+		"tss=document.getElementsByClassName('ts');"
+		"out = document.getElementById('ct').innerHTML;"
+		"lt = Date.now();"
+		""
+		"for (i=0; i<tss.length; i++) {"
+			"e = tss[i];"
+			"var dt =  new Date(lt - parseInt(out) + parseInt(e.innerHTML));"
+			"e.innerHTML = dt.yyyymmdd();"
+		"}"
+		"</script>\r\n"
+		"</body>\r\n</html>\r\n"
+		);
+    delay(1);
+    Serial.println("Client disonnected");
+  }
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+  
+  // Append the last two bytes of the MAC (HEX'd)
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) + String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "Dust-" + macID;
+  AP_NameString.toCharArray(ssid, AP_NameString.length()+1);
+  
+  WiFi.softAP(ssid, password);
+} 
