c5e51869a7391ba0ac202a1072a0115c74880e7e
Yongsoo Han <ilovecho@gmail.com>
2021-05-27 10:38:53 +0900

    Create pid.py

---
+++ Book/RPi3+/Ch.18/pid.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)
+
+while 1:
+    if GPIO.input(18):
+        print ("Detected")
+    else:
+        print ("No Detected")
+    time.sleep(0.5)
+
+GPIO.cleanup()
