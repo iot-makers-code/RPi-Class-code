fbcbdcf1c1348235d1396e72301ba77342ef374b
Yongsoo Han <ilovecho@gmail.com>
2021-05-27 10:23:59 +0900

    Create light.py

---
+++ Book/RPi3+/Ch.9+/light.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+print('Test photo register')
+GPIO.setup(24, GPIO.IN)
+cnt=500
+for i in range(0,cnt,1):
+    inx = GPIO.input(24)
+    time.sleep(0.1)
+    print('input :', inx)
+GPIO.cleanup()
