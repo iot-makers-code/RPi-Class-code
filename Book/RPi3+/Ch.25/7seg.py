4629ea5b88dd4783c0bf652989c7d17ab2c6a01e
Yongsoo Han <ilovecho@gmail.com>
2021-05-27 10:46:43 +0900

    Create 7seg.py

---
+++ Book/RPi3+/Ch.25/7seg.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+pins=[12,16,20,18,23,25,24,21]
+for p in pins: 
+    GPIO.setup(p, GPIO.OUT);
+    GPIO.output(p, 1)
+
+while True:
+    for i in range(len(pins)): 
+        p = pins[i]
+        print p
+        GPIO.output(p, 0)
+        time.sleep(.5)
+        GPIO.output(p, 1)
+        time.sleep(.5)
